fa751035c348a1f0710b0bf6a0f0da38a43c9536
Daniel Portela <danielportela.ti@gmail.com>
2024-11-09 13:13:54 -0300

    Atualizações nos componentes de cadastro, navbar e ajustes de estilo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { RelatoriosComponent } from './pages/relatorios/relatorios.component';
-import { CadastroComponent } from './auth/cadastro/cadastro.component';
 import { HomeComponent } from './pages/home/home.component';
+import { CadastrarUsuarioComponent } from './auth/cadastrar-usuario/cadastrar-usuario.component';
+import { CadastroComponent } from './auth/cadastro/cadastro.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '', pathMatch: 'full' },
   { path: '', component: HomeComponent },
   { path: 'relatorios', component: RelatoriosComponent },
-  { path: 'usuarios-cadastrar', component: CadastroComponent },
+  { path: 'auth/cadastro', component: CadastroComponent },
+  { path: 'auth/cadastrar-usuario', component: CadastrarUsuarioComponent },
 ];
