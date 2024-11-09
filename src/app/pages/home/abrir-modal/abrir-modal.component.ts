40f3e395215b632f3f9c6c087abaf16309060596
Daniel Portela <danielportela.ti@gmail.com>
2024-11-09 13:09:24 -0300

    Atualizações nos componentes de cadastro e navbar, ajustes de estilo e alterações no roteamento.

---
+++ src/app/pages/home/abrir-modal/abrir-modal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-abrir-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './abrir-modal.component.html',
+  styleUrl: './abrir-modal.component.scss',
+})
+export class AbrirModalComponent {}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { AbrirModalComponent } from "./abrir-modal/abrir-modal.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, AbrirModalComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
