7a02053286dc9e0cf14e865e74661e9ac79a3862
Daniel Portela <danielportela.ti@gmail.com>
2024-11-17 10:12:21 -0300

    Reorganizar estrutura de pasta e arquivos

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { AbrirModalComponent } from "./abrir-modal/abrir-modal.component";
+import { LoginModalComponent } from './login-modal/login-modal.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink, AbrirModalComponent],
+  imports: [RouterLink, LoginModalComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
---
+++ src/app/pages/home/login-modal/login-modal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './login-modal.component.html',
+  styleUrl: './login-modal.component.scss',
+})
+export class LoginModalComponent {}
