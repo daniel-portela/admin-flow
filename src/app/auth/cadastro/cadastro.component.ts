b822c5f1589c9c5cd82789bc889d6474f1050df0
Daniel Portela <danielportela.ti@gmail.com>
2024-11-23 15:12:51 -0300

    Refatoração de design e ajustes nos formulários!

--- src/app/auth/cadastro/cadastro.component.ts
+++ src/app/auth/cadastro/cadastro.component.ts
@@ -1,13 +1,39 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro',
   standalone: true,
-  imports: [RouterLink],
+  imports: [ReactiveFormsModule, RouterLink, CommonModule],
   templateUrl: './cadastro.component.html',
-  styleUrl: './cadastro.component.scss'
+  styleUrls: ['./cadastro.component.scss'],
 })
 export class CadastroComponent {
+  cadastroForm: FormGroup;
+  submitted = false;
 
+  constructor(private fb: FormBuilder) {
+    this.cadastroForm = this.fb.group({
+      firstName: ['', [Validators.required, Validators.minLength(3)]],
+      lastName: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      age: ['', [Validators.required, Validators.min(18)]],
+      city: ['', Validators.required],
+      profession: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.cadastroForm.valid) {
+      console.log(this.cadastroForm.value);
+    }
+  }
 }
